6d8df95487bfc1fb06dbed3418e94a0fd636675e
joelchan <chozen86@gmail.com>
2020-05-14 10:35:12 -0400

    add PDF embed url, reformat blocks

--- bib-to-roam.py
+++ bib-to-roam.py
@@ -2,6 +2,7 @@ from tkinter import Tk
 import bibtexparser # dependency, need to pip install
 from bibtexparser.bparser import BibTexParser
 import pyperclip # dependency, need to pip install
+import getPDF_url as gpdf
 
 # get citation from clipboard
 # we assume it is in valid bibtex
@@ -16,6 +17,7 @@ clip_text = r.clipboard_get()
 parser = BibTexParser(common_strings=True)
 bib = bibtexparser.loads(clip_text, parser)
 entry = bib.entries[0]
+print(entry)
 
 # parse title
 title = entry['title'].replace("{", "").replace("}", "")
@@ -36,22 +38,38 @@ entry_type = entry['ENTRYTYPE']
 # key is different for journal articles vs. conference papers; usually I want the series
 et_keys = {
     'article': 'journal',
-    'inproceedings': 'series',
-    'incollection': 'series'
+    'inproceedings': 'booktitle',
+    'incollection': 'booktitle'
+}
+et_source_tags = {
+    'article': '#ref/Paper',
+    'book': '#ref/Book',
+    'incollection': '#ref/BookChapter',
+    'inproceedings': '#ref/Paper'
 }
 publication = "no_info" # default value for other kinds of pubs
-source_type = ""
 if entry_type in et_keys:
     publication = entry[et_keys[entry_type]].replace("{", "").replace("}", "")
-    source_type = "#R-Paper"
+source_tag = ""
+if entry_type in et_source_tags:
+    source_tag = et_source_tags[entry_type]
+else:
+    source_tag = "#ref/Other"
 
 url = ""
 if 'url' in entry:
     url = entry['url']
 
+# get pdf
+# filename = entry['file'].split(":")[0]
+# embed = gpdf.get_pdf_url(filename)
+embed = "Placeholder"
+
 # stitch everything together
-out_str = "- Metadata::\n\t - Title: %s\n\t\t- Tags:: %s\n\t - Authored by:: %s\n\t - Year: %s\n\t - Publication: %s\n\tURL: %s\n\t" %(title, source_type, " , ".join(authors), year, publication, url)
+out_str = "- #references:\n\t- Title: %s\n\t- Meta:\n\t\t- Tags: %s\n\t\t- Authored by:: %s\n\t\t- Year: [[%s]]\n\t\t- Publication: %s\n\t\t- URL: %s\n\t\t- Citekey: %s\n\t- Content\n\t\t- %s" %(title, source_tag, " , ".join(authors), year, publication, url, entry['ID'], embed)
 
 # append to the clipboard
 r.clipboard_clear()
 pyperclip.copy(out_str)
+
+
---
+++ getPDF_url.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+from tkinter import Tk
+import bibtexparser # dependency, need to pip install
+from bibtexparser.bparser import BibTexParser
+import pyperclip # dependency, need to pip install
+import getPDF_url as gpdf
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+def get_pdf_url(filename):
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'gdrive api credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    # results = service.files().list(
+    #     pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    # items = results.get('files', [])
+
+    results = service.files().list(q="name='" + filename + "'",
+        spaces='drive',
+        fields='nextPageToken, files(id, name)').execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+        return None
+    else:
+        print('Files:')
+        pdf_embed = """:hiccup [:iframe {:width "650px", :height "650px", :src "https://drive.google.com/file/d/%s/preview"}]""" %items[0]['id']
+        #for item in items:
+        #    print(u'{0} ({1})'.format(item['name'], item['id']))
+        return pdf_embed
+
+if __name__ == '__main__':
+    # get citation from clipboard
+    # we assume it is in valid bibtex
+    # we assume has title, authors, year, and publication; lazy for now, should add edge cases later
+    r = Tk()
+    r.withdraw()
+    clip_text = r.clipboard_get()
+
+    # parse the bibtex
+    # need to define a parser with custom settings bc zotero has nonstandard bibtex items like "jan" for month
+    # per https://github.com/sciunto-org/python-bibtexparser/issues/192
+    parser = BibTexParser(common_strings=True)
+    bib = bibtexparser.loads(clip_text, parser)
+    entry = bib.entries[0]
+
+    filename = entry['file'].split(":")[0]
+    embed = gpdf.get_pdf_url(filename)
+
+    r.clipboard_clear()
+    pyperclip.copy(embed)
